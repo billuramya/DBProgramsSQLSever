17e6212fc0938b87e8fd63cbaba6d5ad0d9cc003
Ramya billu <billuramya2002@gmail.com>
2024-04-23 15:40:32 +0530

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -11,17 +11,90 @@ CREATE TABLE AuditLog (
     Position VARCHAR(50)
 );
 -- employee
-CREATE TRIGGER AfterInsertEmployee
+alter TRIGGER AfterInsertEmployee
 ON Employees
 AFTER INSERT
 AS
 BEGIN
-    INSERT INTO AuditLog (EventType, EventDateTime, EmployeeID, FirstName, LastName, Position)
-    SELECT 'Insert', GETDATE(), i.EmployeeID, i.FirstName, i.LastName, i.Position
-    FROM inserted i;
+    --INSERT INTO AuditLog (EventType, EventDateTime, EmployeeID, FirstName, LastName, Position)
+    --SELECT 'Insert', GETDATE(), i.EmployeeID, i.FirstName, i.LastName, i.Position
+    --FROM inserted i; 
+	--insert into  Employees(EmployeeID,FirstName,LastName,Position)
+	--select EmployeeID,FirstName,LastName,Position
+	--FROM inserted; 
+	END;
+--UPDATE
+
+ALTER TRIGGER AfterInsertEmployee
+ON Employees
+AFTER INSERT
+AS
+BEGIN
+    UPDATE Employees
+    SET EmployeeID = 3
+    FROM Employees e
+    INNER JOIN inserted i ON e.EmployeeID = i.EmployeeID
+    WHERE i.FirstName = 'Ramya'; 
 END;
 
-INSERT INTO Employees (EmployeeID, FirstName, LastName, Position)
-VALUES (1, 'John', 'Doe', 'Manager'); 
+
+	INSERT INTO Employees (EmployeeID, FirstName, LastName, Position)
+VALUES (2,'Ramya','Billu','HR'); 
+
+use Ramya
+select * from Employees
+DELETE FROM Employees where FirstName='Ramya'
 
 
+
+-- insert operation on triggers
+alter TRIGGER AfterInsertEmployee
+ON Employees
+After Insert
+as
+begin 
+set nocount on ;
+insert into AuditLog(EventType,EventDateTime,EmployeeID,FirstName,LastName,Position)
+select 'Insert',GetDate(),i.EmployeeID,i.FirstName,i.LastName,i.Position from inserted i
+end
+insert into Employees(EmployeeID,FirstName,LastName,Position) values(3,'Lokesh','Ramisetty','Developer')
+
+-- Delete Operation Triggers
+alter TRIGGER AfterInsertEmployee
+ON Employees
+After Delete
+as
+begin 
+set nocount on ;
+insert into AuditLog(EventType,EventDateTime,EmployeeID,FirstName,LastName,Position)
+select 'Delete',GetDate(),i.EmployeeID,i.FirstName,i.LastName,i.Position from Deleted i
+end
+
+delete from Employees where EmployeeID=2;
+
+-- Update the triggers table
+alter TRIGGER AfterInsertEmployee
+ON Employees
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+     Handling INSERT operations
+    IF EXISTS (SELECT * FROM inserted)
+    BEGIN
+        INSERT INTO AuditLog (EventType, EventDateTime, EmployeeID, FirstName, LastName, Position)
+        SELECT 'Insert', GETDATE(), i.EmployeeID, i.FirstName, i.LastName, i.Position
+        FROM inserted i;
+    END
+IF EXISTS (SELECT * FROM deleted)
+    BEGIN
+        INSERT INTO AuditLog (EventType, EventDateTime, EmployeeID, FirstName, LastName, Position)
+        SELECT 'Update', GETDATE(), d.EmployeeID, d.FirstName, d.LastName, d.Position
+        FROM deleted d;
+    END
+end 
+
+update Employees Set Position='CEO' WHERE EmployeeID=3
+
+ SELECT * FROM AuditLog
